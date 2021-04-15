224bb07c8661d50c6b7d0289747c08a4c9d3e9be
eldiar36 <eldiarrd@gnail.com>
2021-04-16 00:37:57 +0600

    edit99

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 
+
 $('.popular-slider').slick({
     infinite: false,
     dots: false,
@@ -12,6 +13,28 @@ $('.popular-slider').slick({
         },
         {
             breakpoint: 800,
+            settings: "slick"
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1.15,
+            }
+        },
+    ]
+});
+$('.cards-wrapper').slick({
+    infinite: false,
+    dots: false,
+    slidesToShow: 1.15,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 3000,
+            settings: "unslick"
+        },
+        {
+            breakpoint: 1023,
             settings: "slick"
         },
         {
@@ -161,34 +184,23 @@ jQuery(document).ready(function ($) {
 
     });
 });
-$('.callback-popup').click(function() {
-    $('.callback-form').show();
-    $('.callback-form').css('display', 'block');
-    $('body').css('overflow', 'hidden');
-
-});
-
-// close modal
-$('.callback-form').click(function() {
-    $(document).on('click', function(event) {
-        var select = $('.callback-form');
-        if ($(event.target).closest(select).length)
-            return;
-        $('body').css('overflow', 'visible');
-        $(' .callback-form').hide();
-
-        $(document).unbind('click');
-        event.stopPropagation();
+$(document).ready(function() {
+    $(".callback-popup").click(function() {
+        $(".callback-container").fadeToggle('fast');
+        $('body').css('overflow', 'hidden');
     });
-});
-$('.call-you').click(function() {
-    $(document).on('click', function(event) {
-        $(' .callback-form').hide();
+    $(".callback-container").bind("click",function(e){
+        if($(e.target).attr("class") != "callback-form")
+            $(".callback-container").fadeOut('fast');
+             $('body').css('overflow', 'visible');
+    })
+    $(".call-you").bind("click",function(e){
+        if($(e.target).attr("class") != "callback-form")
+            $(".callback-container").fadeOut('fast');
         $('body').css('overflow', 'visible');
-        $(document).unbind('click');
-        event.stopPropagation();
-    });
+    })
 });
+
 new universalParallax().init({
     speed: 8.0
 });
@@ -232,70 +244,5 @@ window.addEventListener('mousemove', function(e) {
     let y = e.clientY / window.innerHeight;
     bgthree.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
 });
-(function() {
-    var cards;
-
-    cards = {
-        init: function() {
-            this._cardsWrapper = document.querySelector('.cards-wrapper');
-            this._bindEvents();
-        },
-        _bindEvents: function() {
-            var cards;
-            cards = document.querySelectorAll('.card');
-            [].forEach.call(cards, this._bindEventsToCard.bind(this));
-        },
-        _bindEventsToCard: function(card) {
-            var self;
-            self = this;
-            card.addEventListener('dragstart', (function(e) {
-                self._handleDragStart.call(self, e, this);
-            }), false);
-            card.addEventListener('drag', self._handleDrag.bind(self), false);
-            card.addEventListener('dragend', self._handleDragEnd.bind(self), false);
-        },
-        _resetCard: function(card) {
-            card.removeAttribute('style');
-            card.classList.remove('card--invisible');
-            this._cardsWrapper.insertBefore(card, this._cardsWrapper.firstChild);
-        },
-        _handleDragStart: function(e, card) {
-            var crt;
-            this._dragStart = e.clientX;
-            crt = card.cloneNode(true);
-            crt.style.display = 'none';
-            e.dataTransfer.setDragImage(crt, 0, 0);
-        },
-        _handleDrag: function(e) {
-            var card, dragDirection, dragDistance, maxDistance, newOpacity;
-            dragDistance = e.clientX - this._dragStart;
-            card = e.target;
-            dragDirection = dragDistance < 0 ? 'left' : 'right';
-            maxDistance = dragDistance < 0 ? -250 : 250;
-            newOpacity = maxDistance / 100 * dragDistance / 1000;
-            if (dragDirection === 'right' && dragDistance < 250 || dragDirection === 'left' && dragDistance > -250) {
-                card.setAttribute('style', 'opacity: ' + 1 - newOpacity + '; transform: rotate(' + dragDistance / 50 + 'deg) translateX(' + dragDistance + 'px);');
-            }
-        },
-        _handleDragEnd: function(e) {
-            var card, dragDirection, dragDistance, self;
-            self = this;
-            dragDistance = e.clientX - this._dragStart;
-            card = e.target;
-            dragDirection = dragDistance < 0 ? 'left' : 'right';
-            if (dragDirection === 'right' && dragDistance > 150 || dragDirection === 'left' && dragDistance < -150) {
-                card.classList.add('card--invisible');
-                setTimeout((function() {
-                    self._resetCard(card);
-                }), 200);
-            } else {
-                card.setAttribute('style', 'transform: rotate(0deg) translateX(0px);');
-            }
-        }
-    };
-
-    cards.init();
-
-}).call(this);
 
 
